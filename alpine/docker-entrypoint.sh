fbb2df2b69ce3bf83cbea374f3356cb96199f72b
Moe <github@m03.ca>
2018-02-18 23:58:03 -0800

    Shinobi Docker with SQLite3

--- alpine/docker-entrypoint.sh
+++ alpine/docker-entrypoint.sh
@@ -3,6 +3,13 @@ set -e
 
 cp /opt/shinobi/conf.sample.json /opt/shinobi/conf.json
 cp /opt/shinobi/super.sample.json /opt/shinobi/super.json
+chmod 777 /opt/dbdata/shinobi.sqlite
+if [ ! -e "/opt/dbdata/shinobi.sqlite" ]; then
+    echo "Creating shinobi.sqlite for SQLite3..."
+    cp /opt/shinobi/sql/shinobi.sample.sqlite /opt/dbdata/shinobi.sqlite
+fi
+
+node /opt/shinobi/tools/modifyConfiguration.js databaseType=sqlite3 db='{"filename":"/opt/dbdata/shinobi.sqlite"}'
 
 ADMIN_PASSWORD_MD5=$(echo -n "${ADMIN_PASSWORD}" | md5sum | sed -e 's/  -$//')
 
@@ -13,7 +20,7 @@ sed -i -e 's/"user": "majesticflame"/"user": "'"${MYSQL_USER}"'"/g' \
        -e 's/"database": "ccio"/"database": "'"${MYSQL_DATABASE}"'"/g' \
        "/opt/shinobi/conf.json"
 # Set the admin password
-sed -i -e "s/21232f297a57a5a743894a0e4a801fc3/${ADMIN_PASSWORD_MD5}/" "/opt/shinobi/super.json"
+#sed -i -e "s/21232f297a57a5a743894a0e4a801fc3/${ADMIN_PASSWORD_MD5}/" "/opt/shinobi/super.json"
 
 # Execute Command
 exec "$@"
