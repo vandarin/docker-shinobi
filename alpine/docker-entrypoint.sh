83fe8d5a8258ec2d81bcb5ab14afde2e50e687f8
Moe <github@m03.ca>
2018-05-31 01:46:57 -0700

    alpine rework almost done

--- alpine/docker-entrypoint.sh
+++ alpine/docker-entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 # Copy existing custom configuration files
 echo "Copy custom configuration files ..."
-if [ -d /config ]; then
-    cp -R -f "/config/"* /opt/shinobi || echo "No custom config files found." 
+if [ -d ./config ]; then
+    cp -R -f "./config/"* /opt/shinobi || echo "No custom config files found." 
 fi
 
 # Create default configurations files from samples if not existing
@@ -29,10 +29,35 @@ if [ -n "${ADMIN_PASSWORD}" ]; then
     ADMIN_PASSWORD_MD5=$(echo -n "${ADMIN_PASSWORD}" | md5sum | sed -e 's/  -$//')
 fi
 
+if [ ! -f /var/lib/mysql/ibdata1 ]; then
+
+    mysql_install_db --user=mysql
+
+    /usr/bin/mysqld_safe --user=mysql &
+    sleep 10s
+
+    mysql -u root --password="" <<-EOSQL
+SET @@SESSION.SQL_LOG_BIN=0;
+USE mysql;
+DELETE FROM mysql.user ;
+DROP USER IF EXISTS 'root'@'%','root'@'localhost','${DB_USER}'@'localhost','${DB_USER}'@'%';
+CREATE USER 'root'@'%' IDENTIFIED BY '${DB_PASS}' ;
+CREATE USER 'root'@'localhost' IDENTIFIED BY '${DB_PASS}' ;
+CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}' ;
+CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}' ;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION ;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION ;
+GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%' WITH GRANT OPTION ;
+GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'localhost' WITH GRANT OPTION ;
+DROP DATABASE IF EXISTS test ;
+FLUSH PRIVILEGES ;
+EOSQL
+fi
+
 # Create MySQL database if it does not exists
 if [ -n "${MYSQL_HOST}" ]; then
     echo "Wait for MySQL server" ...
-    while ! mysqladmin ping -h"$MYSQL_HOST" --silent; do
+    while ! mysqladmin ping -h"$MYSQL_HOST"; do
         sleep 1
     done
 fi
@@ -54,16 +79,6 @@ if [ -n "${MYSQL_ROOT_USER}" ]; then
                 "./sql_temp/user.sql"
         fi
 
-        if [ -n "${MYSQL_ROOT_USER}" ]; then
-            if [ -n "${MYSQL_ROOT_PASSWORD}" ]; then
-                echo "Modifying user creation script ..."
-                sed -i -e "s/majesticflame/${MYSQL_USER}/g" \
-                    -e "s/\x27\x27/\x27${MYSQL_PASSWORD}\x27/g" \
-                    -e "s/127.0.0.1/%/g" \
-                    "./sql_temp/user.sql"
-            fi
-        fi
-
         echo "Create database schema if it does not exists ..."
         mysql -u $MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOST -e "source ./sql_temp/framework.sql" || true
 
@@ -76,24 +91,6 @@ fi
 
 # set config data from variables
 echo "Set MySQL configuration from environment variables ..."
-if [ -n "${MYSQL_USER}" ]; then
-    echo "  . Username"
-    sed -i -e 's/"user": "majesticflame"/"user": "'"${MYSQL_USER}"'"/g' \
-        "/opt/shinobi/conf.json"
-fi
-
-if [ -n "${MYSQL_PASSWORD}" ]; then
-    echo "  . Password"
-    sed -i -e 's/"password": ""/"password": "'"${MYSQL_PASSWORD}"'"/g' \
-        "/opt/shinobi/conf.json"
-fi
-
-if [ -n "${MYSQL_HOST}" ]; then
-    echo "  . Host"
-    sed -i -e 's/"host": "127.0.0.1"/"host": "'"${MYSQL_HOST}"'"/g' \
-        "/opt/shinobi/conf.json"
-fi
-
 if [ -n "${MYSQL_DATABASE}" ]; then
     echo "  . Database"
     sed -i -e 's/"database": "ccio"/"database": "'"${MYSQL_DATABASE}"'"/g' \
